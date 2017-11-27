0b385b68e2bf0974bbdf4e38715f6a2e7771e022
LuisLeon <luis290311@gmail.com>
2017-11-26 18:43:06 -0600

    actualizacion al dia 26 de noviembre, seccion ingresos y egresos agregada

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,10 @@ Route::get('/', function () {
 Route::resource('almacen/categoria','CategoriaController');
 Route::resource('almacen/articulo','ArticuloController');
 Route::resource('ventas/cliente','ClienteController');
+Route::resource('ventas/venta','VentaController');
 Route::resource('compras/proveedor','ProveedorController');
 Route::resource('compras/ingreso','IngresoController');
+
 
 Route::get('/home', 'HomeController@index');
 Route::get('/{slug?}', 'HomeController@index');
